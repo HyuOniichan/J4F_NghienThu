269bdb7e6abc449fdcc861c382d8311780eb41d1
Chikine <kiennhc06@gmail.com>
2024-11-27 03:14:42 +0700

    game ne vat the

--- game2/cobac.js
+++
@@ -1,6 +0,0 @@
-        // <img src="./images/dice1dot.gif" alt="Dice 1">
-        // <img src="./images/dice2dot.gif" alt="Dice 2">
-        // <img src="./images/dice3dot.gif" alt="Dice 3">
-        // <img src="./images/dice4dot.gif" alt="Dice 4">
-        // <img src="./images/dice5dot.gif" alt="Dice 5">
-        // <img src="./images/dice6dot.gif" alt="Dice 6"> 
