6f30742e9977aefe70f6ad284d02cbc05abd6bab
Duc Huy <chuahetheky@gmail.com>
2024-12-03 23:30:01 +0700

    [style] update home page UI

--- main.js
+++ main.js
@@ -5,41 +5,60 @@ const $$ = document.querySelectorAll.bind(document);
 
 const gameData = [
     {
-        id: 1, 
-        name: "Catch the Falling Objects", 
-        description: "Made by ChatGPT", 
-        creator: "ChatGPT", 
-        thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyKZN1v55ezQJX_ueiP6YzgAELLGQHpH14kg&s", 
+        id: 1,
+        name: "Catch the Falling Objects",
+        description: "Made by ChatGPT",
+        creator: "ChatGPT",
+        thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyKZN1v55ezQJX_ueiP6YzgAELLGQHpH14kg&s",
         link: "./game1/"
-    }, 
+    },
     {
-        id: 2, 
-        name: "Vua co bac", 
-        description: "Tai xiu chan le?", 
+        id: 2,
+        name: "Vua co bac",
+        description: "Tai xiu chan le?",
         creator: "Kiene",
-        thumbnail: "./game2/images/dice1dot.gif", 
+        thumbnail: "./game2/images/dice1dot.gif",
         link: "./game2/"
-    }, 
+    },
     {
-        id: 3, 
-        name: "Dodge the Obstacles", 
-        description: "Ne vat the", 
+        id: 3,
+        name: "Dodge the Obstacles",
+        description: "Ne vat the",
         creator: "Kiene",
-        thumbnail: "", 
+        thumbnail: "",
         link: "./game3/"
-    }, 
+    },
+    {
+        id: 4,
+        name: "Water sort",
+        description: "test xem ban co bi mu mau khong...",
+        creator: "Kiene",
+        thumbnail: "./game4/thumbnail.png",
+        link: "./game4/"
+    },
+    {
+        id: 5,
+        name: "Ran san moi",
+        description: "tre con thoi nay dau biet nokia la gi",
+        creator: "Kiene",
+        thumbnail: "./game5/image.png",
+        link: "./game5/"
+    },
 ]
 
 const placeholdThumbnail = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1022px-Placeholder_view_vector.svg.png?20220519031949"
 
 const gallery = $('#gallery');
 
-renderGameGallery(); 
+renderGameGallery();
 function renderGameGallery() {
     gallery.innerHTML = gameData.map(game => {
         return `
-            <div class="card" style="width: 18rem;">
-                <img src=${game.thumbnail? game.thumbnail : placeholdThumbnail} class="card-img-top" alt="game_image">
+            <div class="card" style="width: 18rem; min-height: 286px;">
+                <img src="${game.thumbnail ? game.thumbnail : placeholdThumbnail}" 
+                    class="card-img-top" 
+                    alt="game_image"
+                    style="height: 200px; object-fit: cover;">
                 <div class="card-body">
                     <h5 class="card-title">${game.name}</h5>
                     <p class="card-text">${game.description}</p>
@@ -50,6 +69,6 @@ function renderGameGallery() {
             </div>
         `
     }).join('');
-} 
+}
 
 
